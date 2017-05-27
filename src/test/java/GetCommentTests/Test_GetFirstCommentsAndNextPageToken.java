2484e6d9ca0ba322b84498a04e07651a0204b68f
Kaylee <kaylee@posteo.de>
2017-05-27 16:00:11 +0200

    Implemented test case that collects 20 comments

---
+++ src/test/java/GetCommentTests/Test_GetFirstCommentsAndNextPageToken.java
@@ -0,0 +1,54 @@
+package GetCommentTests;
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.Test;
+
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.services.youtube.model.CommentSnippet;
+import com.google.api.services.youtube.model.CommentThread;
+
+import youTubeHelper.YouTubeUtil;
+
+public class Test_GetFirstCommentsAndNextPageToken {
+
+  @Test
+  public void getComments() {
+
+    try {
+      String videoId = "AHQshv_WFWA";
+
+      List<CommentThread> videoComments = YouTubeUtil.getAllComments(videoId);
+
+      if (videoComments.isEmpty()) {
+        System.out.println("Can't get video comments.");
+        fail();
+      } else {
+        // Print information from the API response.
+        System.out.println("Collected: " + videoComments.size() + " comments");
+        System.out.println("\n================== Returned Video Comments ==================\n");
+        for (CommentThread videoComment : videoComments) {
+          CommentSnippet snippet = videoComment.getSnippet().getTopLevelComment().getSnippet();
+          System.out.println("  - Author: " + snippet.getAuthorDisplayName());
+          System.out.println("  - Comment: " + snippet.getTextDisplay());
+          System.out.println("\n-------------------------------------------------------------\n");
+        }
+      }
+
+    } catch (GoogleJsonResponseException e) {
+      System.err.println("GoogleJsonResponseException code: " + e.getDetails().getCode() + " : " + e.getDetails().getMessage());
+      e.printStackTrace();
+      fail();
+    } catch (IOException e) {
+      System.err.println("IOException: " + e.getMessage());
+      e.printStackTrace();
+      fail();
+    } catch (Throwable t) {
+      System.err.println("Throwable: " + t.getMessage());
+      t.printStackTrace();
+      fail();
+    }
+  }
+}
